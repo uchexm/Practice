118babe5f26aafe568445f88f76f59a6f6a1e81c
CHRIS <NWAKAUC1@GMAIL.COM>
2023-07-05 09:25:08 +0200

    es

--- Lesson-02/js/custom.js
+++ Lesson-02/js/custom.js
@@ -1,21 +1,4 @@
-"use strict"
-let something = "hello people";
-let people = {
-  u_name: 1,
-  u_diffname: 2
+let nameBuilder = function( firstName ="John", lastName = "Doe") {
+  console.log( firstName + " " + lastName);
 };
-
-function myFunction(a,b,c){
-  return a+b+c;
-}
-
-document.getElementById("heading").innerHTML = something
-
-document.write("hello uche i am from js")
-
-let myRe = /hello/;
-let myString = "does this include hello world in it";
-
-if(myRe.test(myString)){
-  alert('yes');
-}
+nameBuilder();
