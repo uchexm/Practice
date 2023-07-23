37c9a3440deb3336cd2783297139777bf0165ee7
CHRIS <NWAKAUC1@GMAIL.COM>
2023-07-23 11:50:09 +0200

    custom js

--- Lesson-02/js/custom.js
+++ Lesson-02/js/custom.js
@@ -1,4 +1,9 @@
-let nameBuilder = function( firstName ="John", lastName = "Doe") {
-  console.log( firstName + " " + lastName);
-};
-nameBuilder();
+'hello'
+'hello'
+'$20.95' + 7.99
+'$20.957.99'
+'$' +( 20.95 + 7.99)
+'$28.939999999999998'
+'Items (' + (1+1) + '): $' + (2095 + 799) /100
+'Items (2): $28.94'
+alert('Items (' + (1+1) + '): $' + (2095 + 799) /100);
