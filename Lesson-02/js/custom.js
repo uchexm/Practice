4b6fcab421323c912e2d102b9a24e3e2a46eb7a3
CHRIS <NWAKAUC1@GMAIL.COM>
2023-07-27 13:07:07 +0200

    custom js

--- Lesson-02/js/custom.js
+++ Lesson-02/js/custom.js
@@ -1,16 +1,9 @@
-'hello'
-'hello'
-'$20.95' + 7.99
-'$20.957.99'
-'$' +( 20.95 + 7.99)
-'$28.939999999999998'
-'Items (' + (1+1) + '): $' + (2095 + 799) /100
-'Items (2): $28.94'
-alert('Items (' + (1+1) + '): $' + (2095 + 799) /100);
-
-"I am learning Javascript"
-alert('some\ntext');
-`This is backticks template strings`
-`Items (${1+1}): $${(2095 + 799)/ 100}`;
-`some 
-text`
+//This is a comment.
+      // alert("Hello World!");
+      /*
+       multi
+       line
+       comment
+      */
+       console.log(2 + 2);
+       console.log('some' + 'text');
